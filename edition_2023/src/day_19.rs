8e439b49a7727ceb04439d6623067eef33cb85a1
Rafael Santana <rafaelns.br@gmail.com>
2024-02-13 23:15:39 -0300

    Day 19: part two WIP

--- edition_2023/src/day_19.rs
+++ edition_2023/src/day_19.rs
@@ -1,80 +1,133 @@
 use std::{collections::HashMap, fmt::Debug};
 
+use num::BigInt;
 use regex::{Match, Regex};
 
 pub fn part_one(input: &str) -> isize {
     let program = Program::parse_program(input);
 
-    program.run().iter().map(|part| part.sum()).sum()
+    program.run().iter().map(Part::sum).sum()
+}
+
+pub fn part_two(input: &str) -> BigInt {
+    let program = Program::parse_program(input);
+    let mut possible_checks = Vec::new();
+    program.possible_checks(&mut possible_checks);
+
+    possible_checks
+        .into_iter()
+        .map(|checks| {
+            let mut ranges = vec![1..4001, 1..4001, 1..4001, 1..4001];
+
+            for check in checks {
+                if let Instruction::Less {
+                    prop, to_compare, ..
+                } = check
+                {
+                    ranges[prop.index()].end = to_compare
+                } else if let Instruction::Greater {
+                    prop, to_compare, ..
+                } = check
+                {
+                    ranges[prop.index()].start = to_compare + 1
+                }
+            }
+
+            dbg!(&ranges);
+
+            ranges
+        })
+        .reduce(|mut acc, cur| {
+            for (a, c) in acc.iter_mut().zip(cur.iter()) {
+                if c.start > a.start && c.start < a.end {
+                    a.start = c.start;
+                }
+                if c.end < a.end && c.end > a.start {
+                    a.end = c.end;
+                }
+            }
+
+            acc
+        })
+        .unwrap_or_default()
+        .into_iter()
+        .map(|range| {
+            dbg!(&range);
+            BigInt::from(range.len())
+        })
+        .product::<BigInt>()
 }
 
 #[derive(Debug)]
 struct Program {
-    workflows: Vec<Workflow>,
+    symbol_table: HashMap<String, Workflow>,
     parts: Vec<Part>,
 }
 
 impl Program {
     fn parse_program(string: &str) -> Self {
-        let mut workflows = Vec::new();
+        let mut symbol_table = HashMap::new();
         let mut parts = Vec::new();
 
         for line in string.lines() {
             let line = line.trim();
 
             if let Some(workflow) = Workflow::parse(line) {
-                workflows.push(workflow);
+                symbol_table
+                    .entry(workflow.name.clone())
+                    .or_insert(workflow);
             } else if let Some(part) = Part::parse(line) {
                 parts.push(part);
             }
         }
 
-        Self { workflows, parts }
+        Self {
+            symbol_table,
+            parts,
+        }
     }
 
     fn run(&self) -> Vec<Part> {
         let mut accepted = Vec::new();
-        let symbol_table: HashMap<&str, &Workflow> = self.workflows.iter().fold(
-            HashMap::new(),
-            |mut acc: HashMap<&str, &Workflow>, item| {
-                acc.entry(&item.name).or_insert(item);
-                acc
-            },
-        );
 
-        if let Some(input) = self.workflows.iter().find(|w| w.name == "in") {
+        if let Some(input) = self.symbol_table.get("in") {
             for part in &self.parts {
-                execute_workflow(input, part, &mut accepted, &symbol_table);
+                self.execute_workflow(input, part, &mut accepted);
             }
         }
 
         accepted
     }
-}
 
-fn execute_workflow(
-    current_workflow: &Workflow,
-    part: &Part,
-    accepted: &mut Vec<Part>,
-    symbol_table: &HashMap<&str, &Workflow>,
-) {
-    for rule in &current_workflow.instructions {
-        let next_step = rule.apply(part);
+    fn execute_workflow(&self, current_workflow: &Workflow, part: &Part, accepted: &mut Vec<Part>) {
+        for rule in &current_workflow.instructions {
+            let next_step = rule.apply(part);
 
-        match next_step {
-            NextStep::Continue => (),
-            NextStep::Accept => {
-                accepted.push(part.clone());
-                break;
-            }
-            NextStep::Reject => break,
-            NextStep::Goto(workflow) => {
-                if let Some(&next) = symbol_table.get(workflow.as_str()) {
-                    execute_workflow(next, part, accepted, symbol_table)
+            match next_step {
+                NextStep::Continue => (),
+                NextStep::Accept => {
+                    accepted.push(part.clone());
+                    break;
                 }
-                break;
-            }
+                NextStep::Reject => break,
+                NextStep::Goto(workflow) => {
+                    if let Some(next) = self.symbol_table.get(workflow.as_str()) {
+                        self.execute_workflow(next, part, accepted)
+                    }
+                    break;
+                }
+            };
         }
+    }
+
+    fn possible_checks(&self, possible_checks: &mut Vec<Vec<Instruction>>) {
+        if let Some(input) = self.symbol_table.get("in") {
+            let mut current_checks = Vec::new();
+
+            input.possible_checks(&self.symbol_table, possible_checks, &mut current_checks);
+        }
+
+        dbg!(&possible_checks.len());
     }
 }
 
@@ -130,7 +183,7 @@ impl Part {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum PartProp {
     X,
     M,
@@ -148,12 +201,21 @@ impl PartProp {
             _ => None,
         }
     }
+
+    fn index(&self) -> usize {
+        match self {
+            PartProp::X => 0,
+            PartProp::M => 1,
+            PartProp::A => 2,
+            PartProp::S => 3,
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Workflow {
     name: String,
-    instructions: Vec<Box<dyn Instruction>>,
+    instructions: Vec<Instruction>,
 }
 
 lazy_static! {
@@ -170,162 +232,149 @@ impl Workflow {
 
             Some(Self {
                 name: String::from(name),
-                instructions: instruction_text.flat_map(parse_instruction).collect(),
+                instructions: instruction_text.flat_map(Instruction::parse).collect(),
             })
         } else {
             None
         }
     }
-}
 
-trait Instruction: Debug {
-    fn apply(&self, part: &Part) -> NextStep;
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized;
-}
-
-fn parse_instruction(text: &str) -> Option<Box<dyn Instruction>> {
-    AcceptInstruction::parse(text)
-        .or_else(|| RejectInstruction::parse(text))
-        .or_else(|| GreaterInstruction::parse(text))
-        .or_else(|| LessInstruction::parse(text))
-        .or_else(|| GotoInstruction::parse(text))
-}
-
-#[derive(Debug)]
-struct AcceptInstruction {}
-
-impl Instruction for AcceptInstruction {
-    fn apply(&self, _: &Part) -> NextStep {
-        NextStep::Accept
-    }
-
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized,
-    {
-        if string == "A" {
-            Some(Box::new(AcceptInstruction {}))
-        } else {
-            None
+    fn possible_checks(
+        &self,
+        symbol_table: &HashMap<String, Workflow>,
+        possible_checks: &mut Vec<Vec<Instruction>>,
+        current_checks: &mut Vec<Instruction>,
+    ) {
+        for instruction in &self.instructions {
+            if instruction.possible_checks(
+                instruction,
+                symbol_table,
+                possible_checks,
+                current_checks,
+            ) {
+                break;
+            }
         }
     }
 }
 
-#[derive(Debug)]
-struct RejectInstruction {}
-
-impl Instruction for RejectInstruction {
-    fn apply(&self, _: &Part) -> NextStep {
-        NextStep::Reject
-    }
-
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized,
-    {
-        if string == "R" {
-            Some(Box::new(RejectInstruction {}))
-        } else {
-            None
-        }
-    }
+#[derive(Debug, Clone)]
+enum Instruction {
+    Accept,
+    Reject,
+    Greater {
+        prop: PartProp,
+        to_compare: isize,
+        on_accept: Box<Instruction>,
+    },
+    Less {
+        prop: PartProp,
+        to_compare: isize,
+        on_accept: Box<Instruction>,
+    },
+    Goto {
+        label: String,
+    },
 }
 
-#[derive(Debug)]
-struct GreaterInstruction {
-    prop: PartProp,
-    to_compare: isize,
-    on_accept: Box<dyn Instruction>,
-}
-
-impl Instruction for GreaterInstruction {
+impl Instruction {
     fn apply(&self, part: &Part) -> NextStep {
-        if part.get(&self.prop) > self.to_compare {
-            self.on_accept.apply(part)
-        } else {
-            NextStep::Continue
-        }
-    }
-
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized,
-    {
-        if let Some(caps) = GREATER_REGEX.captures(string) {
-            let prop = PartProp::parse(caps.get(1)?.as_str())?;
-            let to_compare = caps.get(2)?.as_str().parse::<isize>().ok()?;
-            let on_accept = caps
-                .get(3)
-                .and_then(|match_| parse_instruction(match_.as_str()))?;
-
-            Some(Box::new(GreaterInstruction {
+        match self {
+            Instruction::Accept => NextStep::Accept,
+            Instruction::Reject => NextStep::Reject,
+            Instruction::Greater {
                 prop,
                 to_compare,
                 on_accept,
-            }))
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Debug)]
-struct LessInstruction {
-    prop: PartProp,
-    to_compare: isize,
-    on_accept: Box<dyn Instruction>,
-}
-
-impl Instruction for LessInstruction {
-    fn apply(&self, part: &Part) -> NextStep {
-        if part.get(&self.prop) < self.to_compare {
-            self.on_accept.apply(part)
-        } else {
-            NextStep::Continue
-        }
-    }
-
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized,
-    {
-        if let Some(caps) = LESS_REGEX.captures(string) {
-            let prop = PartProp::parse(caps.get(1)?.as_str())?;
-            let to_compare = caps.get(2)?.as_str().parse::<isize>().ok()?;
-            let on_accept = caps
-                .get(3)
-                .and_then(|match_| parse_instruction(match_.as_str()))?;
-
-            Some(Box::new(LessInstruction {
+            } => {
+                if part.get(prop) > *to_compare {
+                    on_accept.apply(part)
+                } else {
+                    NextStep::Continue
+                }
+            }
+            Instruction::Less {
                 prop,
                 to_compare,
                 on_accept,
-            }))
-        } else {
-            None
+            } => {
+                if part.get(prop) < *to_compare {
+                    on_accept.apply(part)
+                } else {
+                    NextStep::Continue
+                }
+            }
+            Instruction::Goto { label } => NextStep::Goto(label.clone()),
         }
     }
-}
 
-#[derive(Debug)]
-struct GotoInstruction {
-    label: String,
-}
+    fn parse(text: &str) -> Option<Instruction> {
+        if text == "A" {
+            return Some(Instruction::Accept);
+        } else if text == "R" {
+            return Some(Instruction::Reject);
+        }
 
-impl Instruction for GotoInstruction {
-    fn apply(&self, _: &Part) -> NextStep {
-        NextStep::Goto(self.label.clone())
+        if let Some(caps) = GREATER_REGEX.captures(text) {
+            let prop = PartProp::parse(caps.get(1)?.as_str())?;
+            let to_compare = caps.get(2)?.as_str().parse::<isize>().ok()?;
+            let on_accept = Box::new(Instruction::parse(caps.get(3)?.as_str())?);
+
+            return Some(Instruction::Greater {
+                prop,
+                to_compare,
+                on_accept,
+            });
+        }
+
+        if let Some(caps) = LESS_REGEX.captures(text) {
+            let prop = PartProp::parse(caps.get(1)?.as_str())?;
+            let to_compare = caps.get(2)?.as_str().parse::<isize>().ok()?;
+            let on_accept = Box::new(Instruction::parse(caps.get(3)?.as_str())?);
+
+            return Some(Instruction::Less {
+                prop,
+                to_compare,
+                on_accept,
+            });
+        }
+
+        Some(Instruction::Goto {
+            label: String::from(text),
+        })
     }
 
-    fn parse(string: &str) -> Option<Box<dyn Instruction>>
-    where
-        Self: Sized,
-    {
-        Some(Box::new(GotoInstruction {
-            label: String::from(string),
-        }))
+    fn possible_checks(
+        &self,
+        instruction: &Instruction,
+        symbol_table: &HashMap<String, Workflow>,
+        possible_checks: &mut Vec<Vec<Instruction>>,
+        current_checks: &mut Vec<Instruction>,
+    ) -> bool {
+        match self {
+            Instruction::Accept => {
+                possible_checks.push(current_checks.clone());
+                return true;
+            }
+            Instruction::Reject => (), // Do nothing for Reject
+            Instruction::Greater { on_accept, .. } | Instruction::Less { on_accept, .. } => {
+                let mut divergent_checks = current_checks.clone();
+                divergent_checks.push(instruction.clone());
+                on_accept.possible_checks(
+                    &on_accept,
+                    symbol_table,
+                    possible_checks,
+                    &mut divergent_checks,
+                );
+            }
+            Instruction::Goto { label } => {
+                if let Some(next) = symbol_table.get(label.as_str()) {
+                    next.possible_checks(symbol_table, possible_checks, current_checks)
+                }
+            }
+        };
+
+        false
     }
 }
 
@@ -355,6 +404,10 @@ mod tests {
         {x=2127,m=1623,a=2188,s=1013}";
 
         assert_eq!(19114, part_one(input));
+        assert_eq!(
+            BigInt::parse_bytes(b"167409079868000", 10).unwrap(),
+            part_two(input)
+        );
     }
 
     #[test]
@@ -362,5 +415,9 @@ mod tests {
         let input = include_str!("../res/day_19.txt");
 
         assert_eq!(575412, part_one(input));
+        assert_eq!(
+            BigInt::parse_bytes(b"167409079868000", 10).unwrap(),
+            part_two(input)
+        );
     }
 }
